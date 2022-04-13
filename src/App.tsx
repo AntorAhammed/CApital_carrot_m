b16c90497fc72ebaf7b9859e6e98baf46bfb9210
coders003 <94458357+coders003@users.noreply.github.com>
2022-04-13 07:26:00 +0530

    mainnet update

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 import Admin from "./screens/admin/Admin";
 
 function App() {
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.mainnet-beta;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(() => [getPhantomWallet()], []);
 
